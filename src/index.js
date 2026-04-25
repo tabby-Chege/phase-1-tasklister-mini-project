a48d7bd4b2612548e4d838003b5b8137b2dfd343
tabbychege <tabbywam98@gmail.com>
2026-04-25 21:43:17 +0300

    updated file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const task = document.getElementById("new-task-description").value;
+buildToDo(task);
 });
+});
+function buildToDo(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+  document.getElementById("tasks").appendChild(li);
+}
